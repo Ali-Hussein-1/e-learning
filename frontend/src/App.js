ea4aca14608160154cc4f85766be0bb091e0363c
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-10 00:03:52 +0300

    Frontend<>Creating and styling SidePanel component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import SignIn from "./components/SignIn";
 import Button from "./components/Button";
+import SidePanel from "./components/SidePanel";
 
 
 function App() {
   return (
     <>
-      {" "}
+      {/* {" "}
       <div className="App">
         <SignIn/>
+      </div> */}
+      <div className="App">
+        <SidePanel/>
       </div>
     </>
   );
