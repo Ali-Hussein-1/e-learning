b77ed2baff0cb4fac67abd127e723147dd9ea92d
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-10 03:41:01 +0300

    Frontend<>Creating and styling SidePanelCards component

--- frontend/src/components/SidePanel.jsx
+++ frontend/src/components/SidePanel.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import SidePanelCards from "./SidePanelCards";
 
 const SidePanel = () => {
-  return <div className="sidepanel">ADMIN</div>;
+  return (
+    <>
+      <div className="sidepanel">
+        <h1>ADMIN</h1>
+        <SidePanelCards data={"Instructors"} />
+        <SidePanelCards data={"Students"} />
+        <SidePanelCards data={"Courses"} />
+      </div>
+      ;
+    </>
+  );
 };
 
 export default SidePanel;
---
+++ frontend/src/components/SidePanelCards.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SidePanelCards = ({ data }) => {
+  return <div className="sidepanelcards">{data}</div>;
+};
+
+export default SidePanelCards;
