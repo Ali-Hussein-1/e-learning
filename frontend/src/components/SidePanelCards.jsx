981bf48cfd252b969fbc200b45c1af26de7fa588
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-10 04:03:38 +0300

    Frontend<>Restyling the side panel cards

--- frontend/src/components/SidePanelCards.jsx
+++ frontend/src/components/SidePanelCards.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { FaChalkboardTeacher } from "react-icons/fa";
 
 const SidePanelCards = ({ data }) => {
-  return <div className="sidepanelcards">{data}</div>;
+  return (
+    <div className="sidepanelcards">
+      <div>
+        <FaChalkboardTeacher />
+      </div>
+      <div>{data}</div>
+    </div>
+  );
 };
 
 export default SidePanelCards;
