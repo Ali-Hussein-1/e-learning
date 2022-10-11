edc65d5acb4b0fcab748c6097e9f5ebd3fdaa9d8
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-10-11 23:34:05 +0300

    Backend<>Creating an StudentMiddleware and regrouping Routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,7 +17,7 @@ Route::controller(AuthController::class)->group(function () {
     
 });
 
-Route::group(['prefix' => 'admin'], function () {
+Route::group(["prefix" => "v1"], function () {
     Route::group(['middleware' => 'admin.role'], function () {
         Route::post('/register',[AuthController::class,'register']);
         Route::post("/addcourse", [CourseController::class, "AddCourse"]);
@@ -25,14 +25,19 @@ Route::group(['prefix' => 'admin'], function () {
 });
 
 
-Route::group(["prefix"=> "v1"], function(){
+Route::group(["prefix"=> "v2"], function(){
     Route::group(['middleware' => 'instructor.role'], function (){
         Route::post("/addanncmnt", [AnnouncementController::class, "AddAnnouncement"]);
         Route::post("/addassignment", [AssignmentController::class, "AddAssignment"]);
     });
 });
     
+Route::group(["prefix"=> "v3"], function(){
+    Route::group(['middleware' => 'student.role'], function (){
+        Route::get("/viewanncmnt", [AnnouncementController::class, "ViewAnnouncements"]);
+        Route::get("/viewassignments", [AssignmentController::class, "viewAssignments"]);
+    });
+});
    
-    Route::get("/viewanncmnt", [AnnouncementController::class, "ViewAnnouncements"]);
-    Route::get("/viewassignments", [AssignmentController::class, "viewAssignments"]);
+    
 
